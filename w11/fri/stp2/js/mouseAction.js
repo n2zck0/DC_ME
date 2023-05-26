6fb4d18aea9f8e306b61f3afaaa87d2456514c01
n2zck0 <126849549+n2zck0@users.noreply.github.com>
2023-05-26 14:21:27 +0900

    vb

---
+++ w11/fri/stp2/js/mouseAction.js
@@ -0,0 +1,20 @@
+//let box = document.querySelector("#box2");
+//box.onclick = () => {
+//  box.classList.toggle("boxSelected");
+//};
+
+let boxes = document.querySelectorAll(".box");
+console.log(boxes);
+console.log(boxes[0]);
+console.log(boxes[1]);
+console.log(boxes[2]);
+//boxes.forEach(function (h) {
+//  h.onclick = () => {
+//    h.classList.toggle("boxSelected");
+//  };
+//});
+boxes.forEach((each) => {
+  each.onclick = () => {
+    each.classList.toggle("boxSelected");
+  };
+});
